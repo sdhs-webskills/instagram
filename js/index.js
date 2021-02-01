8c8928adf0a5046a00bd3d07eac5573f14dc42e2
minsung03 <sdh190303@sdh.hs.kr>
2021-02-01 15:26:42 +0900

    feat: complete mission 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // write code...
+
+window.onload = function() {
+    const hearts = document.querySelectorAll(".heart");
+    const activeMenus = document.querySelectorAll(".content-active-menu");
+    hearts.forEach((heart, index) => {
+        heart.addEventListener("click", () => {
+
+            document.body.insertAdjacentHTML("beforeend", `
+            <div class="heart-animation">
+                <img src="images/heart_red.png">
+            </div>`);
+
+            setTimeout(() => {
+                document.body.removeChild(document.querySelector(".heart-animation"));
+            }, 1350);
+
+            heart.setAttribute("src", "images/heart_red.png");
+
+            const likeCount = activeMenus[index].querySelectorAll("span")[0];
+            
+            likeCount.innerHTML = Number(likeCount.innerHTML) + 1;
+        });
+    });
+};
