b6d4aa99e7f7b3d1de63317efe6507ac0708a544
wonsunil <seonil6352@gmail.com>
2021-02-01 15:20:06 +0900

    Feat: heart click event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// write code...
+window.onload = () => {
+	document.body.onclick = ({ target }) => {
+		if(!target.classList.contains("heart")) return false;
+
+		document.body.insertAdjacentHTML(
+			"beforeend",
+			"<div class=heart-animation><img src='images/heart_red.png'></div>"
+			);
+
+		target.src = "images/heart_red.png";
+
+		const heartSpan = target.parentNode.children[1];
+
+		heartSpan.innerHTML = Number(heartSpan.innerHTML) + 1;
+
+		setTimeout(() => {
+			document.body.removeChild(document.querySelector(".heart-animation"));
+		}, 1350);
+	};
+};
