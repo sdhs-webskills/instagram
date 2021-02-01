a9c22fa2f60e3fef0cb29e114c53c2ccff5f0333
wonsunil <seonil6352@gmail.com>
2021-02-01 15:40:29 +0900

    Feat: save/remove clicked heart index in localStorage

--- js/index.js
+++ js/index.js
@@ -1,4 +1,16 @@
 window.onload = () => {
+	const findFromLocalStorage = index => {
+		const ids = JSON.parse(localStorage.getItem("heart")) ?? {id: []};
+
+		return ids["id"].findIndex(id => id === index);
+	};
+	const setState = (newState) => {
+		const todos = JSON.parse(localStorage.getItem("heart")) || {id: []};
+		todos["id"].push(newState["id"]);
+
+		localStorage.setItem("heart", JSON.stringify(todos));
+	};
+
 	document.body.onclick = ({ target }) => {
 		if(!target.classList.contains("heart")) return false;
 
@@ -16,5 +28,22 @@ window.onload = () => {
 		setTimeout(() => {
 			document.body.removeChild(document.querySelector(".heart-animation"));
 		}, 1350);
+
+		let contentIndex = null;
+
+		[...document.querySelectorAll(".content")].forEach((element, index) => {
+			if(element === target.parentNode.parentNode.parentNode) return contentIndex = index;
+		});
+
+
+		if(findFromLocalStorage(contentIndex) < 0)
+			return setState({id: contentIndex});
+
+		const array = JSON.parse(localStorage.getItem("heart"))["id"];
+		array.splice(findFromLocalStorage(contentIndex), 1);
+
+		localStorage.setItem("heart", JSON.stringify({id: array}));
+
+		console.log(localStorage);
 	};
 };
