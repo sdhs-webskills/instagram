c2f2a3d52d4eb640271c069180846962d12921ce
wonsunil <seonil6352@gmail.com>
2021-02-05 16:03:37 +0900

    Feat: login page

---
+++ src/pages/user/account/login.php
@@ -0,0 +1,18 @@
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>register</title>
+    <style>
+        *{ margin: 0; padding: 0;}
+        body{ background: #ededed; }
+    </style>
+    <link rel="stylesheet" href="../../../resources/css/form.css">
+</head>
+<body>
+<form action="../../../action/login.php" method="POST">
+    <input type="text" name="email" class="form-first-input" placeholder="이메일" autocomplete="off">
+    <input type="password" name="password" class="form-last-input" placeholder="비밀번호">
+    <input type="submit" value="로그인">
+</form>
+</body>
+</html>
