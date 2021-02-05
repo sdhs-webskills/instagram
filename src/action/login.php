ed0c08716f63818cd3db133f6cb5016e280d1a78
wonsunil <seonil6352@gmail.com>
2021-02-05 16:03:05 +0900

    Feat: embody login logic

---
+++ src/action/login.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once("../model/user.php");
+include_once("../lib/function.php");
+include_once("../core/DB.php");
+
+use \src\core\DB;
+
+if(session_status() == PHP_SESSION_NONE) session_start();
+
+if($_SERVER["REQUEST_METHOD"] !== "POST") header("Location: http://localhost/instagram/src/main.php");
+
+extract($_POST);
+$result = DB::fetch("select * from user_info where email = ? and password = ?", array($email, md5($password)));
+
+$_SESSION["user"] = serialize(new User($result[0], $result[1], $result[2], $result[3]));
+
+alert("로그인되었습니다");
+move("/instagram/src/main.php");
