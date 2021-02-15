caf423d233902e99335f3903de6e6bcbd7b883c0
wonsunil <seonil6352@gmail.com>
2021-02-15 11:37:52 +0900

    Refactor: change dom tree

--- src/main.php
+++ src/main.php
@@ -8,123 +8,127 @@
 </head>
 <body>
 	<article id="wrap">
-		<section id="stories"></section>
-		<section id="menu">
-			
-		</section>
-		<section id="contents">
-			<div class="content-line">
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/1.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/2.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/3.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="content-line">
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/4.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/5.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/6.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="content-line">
-				<div class="content">
-					<div class="content-picture-box">
-						<img src="resources/images/7.jpg" alt="" class="content-picture">
-					</div>
-					<div class="content-menu">
-						<div class="content-profile">
-							<div class="profile-img"></div>
-							<div class="profile-name"></div>
-						</div>
-						<div class="content-active-menu">
-							<img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
-							<img src="resources/images/comment.png" alt=""><span>0 </span>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<section id="content">
+            <section id="stories">
+                <h2>Stories</h2>
+            </section>
+            <section id="contents">
+                <h2>Feed</h2>
+                <div id="content-line-box">
+                    <div class="content-line">
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/1.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/2.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/3.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content-line">
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/4.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/5.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/6.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="content-line">
+                        <div class="content">
+                            <div class="content-picture-box">
+                                <img src="resources/images/7.jpg" alt="" class="content-picture">
+                            </div>
+                            <div class="content-menu">
+                                <div class="content-profile">
+                                    <div class="profile-img"></div>
+                                    <div class="profile-name"></div>
+                                </div>
+                                <div class="content-active-menu">
+                                    <img src="resources/images/heart.png" alt="" class="heart"><span>0 </span>
+                                    <img src="resources/images/comment.png" alt=""><span>0 </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </section>
 	</article>
 </body>
 </html>
