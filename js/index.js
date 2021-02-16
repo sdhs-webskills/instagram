406f8c8010addf693100d6712d8b881d55f075df
minsung03 <sdh190303@sdh.hs.kr>
2021-02-16 14:15:40 +0900

    Revoke file delete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// write code...
+window.onload = function() {
+    const hearts = document.querySelectorAll(".heart");
+    const activeMenus = document.querySelectorAll(".content-active-menu");
+    hearts.forEach((heart, index) => {
+        heart.addEventListener("click", () => {
+
+            document.body.insertAdjacentHTML("beforeend", `
+            <div class="heart-animation">
+                <img src="images/heart_red.png">
+            </div>`);
+
+            setTimeout(() => {
+                document.body.removeChild(document.querySelector(".heart-animation"));
+            }, 1350);
+
+            heart.setAttribute("src", "images/heart_red.png");
+
+            const likeCount = activeMenus[index].querySelectorAll("span")[0];
+
+            likeCount.innerHTML = Number(likeCount.innerHTML) + 1;
+
+            localStorage.setItem("index",JSON.stringify(index));
+            localStorage.getItem("index",heart);
+
+            console.log(localStorage.getItem("index", JSON.stringify(heart)));
+        });
+    });
+
+};
