a51588f2a0c0cb7dc1c1a9391088e9830ea7ab04
minsung03 <sdh190303@sdh.hs.kr>
2021-02-16 16:09:07 +0900

    Feat: register data validator checking

---
+++ View/join.js
@@ -0,0 +1,6 @@
+const email = document.querySelector(".email");
+
+function chkEmail() {
+    let regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
+
+}
