aa12081311088f88825a02223a5c37cb8fc3df81
minsung03 <sdh190303@sdh.hs.kr>
2021-02-16 16:10:18 +0900

    Feat: embody register page

--- View/join.php
+++ View/join.php
@@ -6,11 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <script src="./join.js"></script>
 </head>
 <body>
     <div id="join">
-        <input type="email" class="email">
-        <input type="password" class="password">
+        <h2>회원가입</h2>
+        <input type="email" class="email" placeholder="이메일을 입력하세요">
+        <input type="password" class="password" placeholder="비밀번호를 입력하세요">
+        <input type="password" class="password_chk" placeholder="비밀번호를 확인하세요">
+        <input type="submit" value="회원가입">
     </div>
 </body>
 </html>
