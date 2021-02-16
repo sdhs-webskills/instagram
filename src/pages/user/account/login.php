a1010fdb3f312016e03389198052be35075b9d7d
wonsunil <seonil6352@gmail.com>
2021-02-16 16:41:53 +0900

    Feat: change browser title register to login

--- src/pages/user/account/login.php
+++ src/pages/user/account/login.php
@@ -1,7 +1,7 @@
 <html lang="ko">
 <head>
     <meta charset="UTF-8">
-    <title>register</title>
+    <title>login</title>
     <style>
         *{ margin: 0; padding: 0;}
         body{ background: #ededed; }
