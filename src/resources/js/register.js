63c3caca6a18a2cc051ff1dd3be7b498e91f22a6
wonsunil <seonil6352@gmail.com>
2021-02-18 11:04:01 +0900

    Feat: add phone number check reg

--- src/resources/js/register.js
+++ src/resources/js/register.js
@@ -11,11 +11,13 @@ $form.addEventListener("submit", event => {
     const $password = $form.children[2];
     const $phone = $form.children[3];
 
-    const emailCheck = $email.value.match(new RegExp(/[A-Za-z0-9]*@[A-Za-z]([.]?[a-zA-Z])*\.[a-zA-Z]{2,3}/g));
-    const nameCheck = $name.value.match(new RegExp(/^[ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z]{2,10}$/g));
-    const passwordCheck = $password.value.match(new RegExp(/^[A-Za-z0-9!@#$%^&*()]*$/g));
-    const phoneCheck = $phone.value.match();
+    const emailCheck = $email.value.match(new RegExp(/[A-Za-z0-9]*@[A-Za-z]([.]?[a-zA-Z])*\.[a-zA-Z]{2,3}/));
+    const nameCheck = $name.value.match(new RegExp(/[ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z]{2,10}/));
+    const passwordCheck = $password.value.match(new RegExp(/[A-Za-z0-9!@#$%^&*()]*/));
+    const phoneCheck = $phone.value.match(new RegExp(/[0-9]*([-]?[0-9]{3,4}?[-]?[0-9]{4})/));
 
     console.log(emailCheck);
     console.log(nameCheck);
+    console.log(passwordCheck);
+    console.log(phoneCheck);
 });
