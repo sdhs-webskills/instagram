7734385cc7d0218afcc7fb04beb2c32a61c63fc1
wonsunil <seonil6352@gmail.com>
2021-02-18 16:01:43 +0900

    Feat: embody validate check function

--- src/resources/js/register.js
+++ src/resources/js/register.js
@@ -1,5 +1,16 @@
-const dataValidateCheck = () => {
+const dataValidateCheck = ($form) => {
+    const $email = $form.children[0];
+    const $name = $form.children[1];
+    const $password = $form.children[2];
+    const $phone = $form.children[3];
 
+    const emailCheck = $email.value.match(new RegExp(/[A-Za-z0-9]*@[A-Za-z]([.]?[a-zA-Z])*\.[a-zA-Z]{2,3}/));
+    const nameCheck = $name.value.match(new RegExp(/[ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z]{2,10}/));
+    const passwordCheck = $password.value.match(new RegExp(/[A-Za-z0-9!@#$%^&*()]*/));
+    const phoneCheck = $phone.value.match(new RegExp(/[0-9]*([-]?[0-9]{3,4}?[-]?[0-9]{4})/));
+    const checkResult = [emailCheck, nameCheck, passwordCheck, phoneCheck];
+
+    return checkResult.filter(check => check === null || check?.input === "");
 };
 
 const $form = document.forms[0];
