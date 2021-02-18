dbb67d550ed8b00c8045250b2848f18bc1866ea7
wonsunil <seonil6352@gmail.com>
2021-02-18 16:02:13 +0900

    Feat: submit method

--- src/resources/js/register.js
+++ src/resources/js/register.js
@@ -22,13 +22,5 @@ $form.addEventListener("submit", event => {
         return alert(`${checkResult[i].placeholder}칸이 형식에 맞지 않습니다`);
     }
 
-    const emailCheck = $email.value.match(new RegExp(/[A-Za-z0-9]*@[A-Za-z]([.]?[a-zA-Z])*\.[a-zA-Z]{2,3}/));
-    const nameCheck = $name.value.match(new RegExp(/[ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z]{2,10}/));
-    const passwordCheck = $password.value.match(new RegExp(/[A-Za-z0-9!@#$%^&*()]*/));
-    const phoneCheck = $phone.value.match(new RegExp(/[0-9]*([-]?[0-9]{3,4}?[-]?[0-9]{4})/));
-
-    console.log(emailCheck);
-    console.log(nameCheck);
-    console.log(passwordCheck);
-    console.log(phoneCheck);
+    return $form.submit();
 });
