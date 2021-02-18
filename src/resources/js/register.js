ebc82c8cc6df4a322710cebca7842226104c0d86
wonsunil <seonil6352@gmail.com>
2021-02-18 16:02:00 +0900

    Feat: validate error alter

--- src/resources/js/register.js
+++ src/resources/js/register.js
@@ -17,10 +17,10 @@ const $form = document.forms[0];
 $form.addEventListener("submit", event => {
     event.preventDefault();
 
-    const $email = $form.children[0];
-    const $name = $form.children[1];
-    const $password = $form.children[2];
-    const $phone = $form.children[3];
+    const checkResult = dataValidateCheck(event.target);
+    for(let i = 0; i < checkResult.length; i++) {
+        return alert(`${checkResult[i].placeholder}칸이 형식에 맞지 않습니다`);
+    }
 
     const emailCheck = $email.value.match(new RegExp(/[A-Za-z0-9]*@[A-Za-z]([.]?[a-zA-Z])*\.[a-zA-Z]{2,3}/));
     const nameCheck = $name.value.match(new RegExp(/[ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z]{2,10}/));
