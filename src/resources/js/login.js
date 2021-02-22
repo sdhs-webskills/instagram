415e6ef0654ff6fd1765cbdd512296dfde4a2a38
wonsunil <seonil6352@gmail.com>
2021-02-22 13:30:48 +0900

    Fix: typo error

--- src/resources/js/login.js
+++ src/resources/js/login.js
@@ -2,5 +2,9 @@ const $form = document.forms[0];
 $form.addEventListener("submit", event => {
     event.preventDefault();
 
+    const checkResult = dataValidateCheck(event.target);
+    for(let i = 0; i < checkResult.length; i++) {
+        return alert(`${document.querySelectorAll("input")[i].placeholder}칸이 형식에 맞지 않습니다`);
+    }
 
 });
