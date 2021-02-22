c457d36a08f68d36a5b7d3f993b775fa6ce875ca
wonsunil <seonil6352@gmail.com>
2021-02-22 13:31:04 +0900

    Feat: input data validate check

--- src/resources/js/login.js
+++ src/resources/js/login.js
@@ -1,3 +1,14 @@
+const dataValidateCheck = $form => {
+    const $email = $form.children[0];
+    const $password = $form.children[1];
+
+    const emailCheck = $email.value.match(new RegExp(/[A-Za-z0-9]*@[A-Za-z]([.]?[a-zA-Z])*\.[a-zA-Z]{2,3}/));
+    const passwordCheck = $password.value.match(new RegExp(/[A-Za-z0-9!@#$%^&*()]*/));
+    const checkResult = [emailCheck, passwordCheck];
+
+    return checkResult.filter(check => check === null || check?.input === "");
+};
+
 const $form = document.forms[0];
 $form.addEventListener("submit", event => {
     event.preventDefault();
@@ -7,4 +18,5 @@ $form.addEventListener("submit", event => {
         return alert(`${document.querySelectorAll("input")[i].placeholder}칸이 형식에 맞지 않습니다`);
     }
 
+    return $form.submit();
 });
