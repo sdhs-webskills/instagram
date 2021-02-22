b7aa9b563c7be01e1855c3a95d8e8947dd0529ca
wonsunil <seonil6352@gmail.com>
2021-02-22 13:30:26 +0900

    Feat: user data exist check

--- src/action/login.php
+++ src/action/login.php
@@ -14,7 +14,12 @@ extract($_POST);
 
 $result = DB::fetch("select * from user_info where email = ? and password = ?", array($email, md5($password)));
 
-$_SESSION["user"] = serialize(new User($result[0], $result[1], $result[2], $result[3]));
+if($result) {
+    $_SESSION["user"] = serialize(new User($result[0], $result[1], $result[2], $result[3]));
 
-alert("로그인되었습니다");
-move("/instagram/src/main.php");
+    alert("로그인되었습니다");
+    move("/instagram/src/main.php");
+}else{
+    alert("이메일 또는 비밀번호가 잘못되었습니다");
+    move("../pages/user/account/login.php");
+};
