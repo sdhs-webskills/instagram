f26620ebb0cd224bf400894e2a599f0a506d2158
wonsunil <seonil6352@gmail.com>
2021-02-26 13:12:13 +0900

    Add: profile php file

---
+++ src/pages/user/profile.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>profile</title>
+    <link rel="stylesheet" href="../../resources/css/profile.css">
+</head>
+<body>
+<article id="wrap">
+    <section id="profile">
+        <div id="profile-img">
+            <img src="../../resources/images/basic_img.jpg" alt="">
+        </div>
+        <div id="introdution" contenteditable="true">
+
+        </div>
+    </section>
+    <section id="contents">
+        <div class="content">
+            <div class="content-picture-box">
+                <img src="../../resources/images/1.jpg" alt="" class="content-picture">
+            </div>
+        </div>
+    </section>
+</article>
+</body>
+</html>
