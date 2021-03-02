667ade9ff60962abd84727ba3ab0f9c7cc502bc5
minsung03 <sdh190303@sdh.hs.kr>
2021-03-02 16:44:55 +0900

    feat: bubbling event

--- View/profile.js
+++ View/profile.js
@@ -3,6 +3,10 @@ window.onload = function() {
     const $inputFile = document.querySelector("input[type='file']");
     const $profileImage = document.querySelector("#profile-img > img");
     const $introduction = document.querySelector("#introduction");
+    const $introductionButtonBox = document.querySelector("#introduction-button-box");
+    const $edit = document.querySelector("#edit");
+    const $save = document.querySelector("#save");
+    const $reset = document.querySelector("#reset");
 
     $profileChange.addEventListener("click", () => {
         $inputFile.click();
@@ -19,5 +23,33 @@ window.onload = function() {
         $profileImage.src = URL.createObjectURL(selectedFile);
     });
 
+    const addClickEvent = (target, listener) => {
+        return target.addEventListener("click", listener);
+    };
 
+    addClickEvent($introductionButtonBox, ({ target }) => {
+        if(target.id === "edit") {
+            $introduction.contentEditable  = true;
+            return $introduction.parentNode.children[1].innerHTML = `           
+                <button id="save">저장</button>
+                <button id="reset">초기화</button>
+            `;
+        };
+
+        if(target.id === "save") {
+            $introductionButtonBox.innerHTML = `<button id="edit">수정</button>`;
+
+            return $introduction.contentEditable = false;
+        };
+
+        if(target.id === "reset") {
+            $introduction.innerHTML = `
+             아직 자기소개를 작성하지 않았습니다.
+            `;
+
+            $introductionButtonBox.innerHTML = `<button id="edit">수정</button>`;
+
+            return $introduction.contentEditable = false;
+        };
+    });
 }
