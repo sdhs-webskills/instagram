d162c4fbe6a279db0984e2405d7bed9c2f24e4a7
wonsunil <seonil6352@gmail.com>
2021-03-08 15:56:46 +0900

    Add: private variables and getter & setter

--- src/model/user.php
+++ src/model/user.php
@@ -3,14 +3,16 @@
 class User{
     private string $email;
     private string $name;
-    private string $password;
     private string $phone;
+    private string $image;
+    private string $introduction;
 
-    public function __construct(string $email, string $name, string $password, string $phone) {
+    public function __construct(string $email, string $name, string $phone, string $image, string $introduction) {
         $this -> email = $email;
         $this -> name = $name;
-        $this -> password = $password;
         $this -> phone = $phone;
+        $this -> image = $image;
+        $this -> introduction = $introduction;
     }
 
     public function getEmail(): string {
@@ -21,11 +23,35 @@ class User{
         return $this -> name;
     }
 
-    public function getPassword(): string {
-        return $this -> password;
-    }
-
     public function getPhone(): string {
         return $this -> phone;
+    }
+
+    public function getImage(): string {
+        return $this -> image;
+    }
+
+    public function getIntroduction(): string {
+        return $this -> introduction;
+    }
+
+    public function setEmail(string $email) {
+        $this -> email = $email;
+    }
+
+    public function setName(string $name) {
+        $this -> name = $name;
+    }
+
+    public function setPhone(string $phone) {
+        $this -> phone = $phone;
+    }
+
+    public function setImage(string $image) {
+        $this -> image = $image;
+    }
+
+    public function setIntroduction(string $introduction) {
+        $this -> introduction = $introduction;
     }
 }
